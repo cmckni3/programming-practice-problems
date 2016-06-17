fd161f1b8ec2ac103bd917d2d540a864efc5cb97
Chris McKnight <cmckni3@gmail.com>
2016-06-17 10:43:54 -0500

    Adds hacker rank solutions

---
+++ hacker-rank/18-queues-stacks/main.js
@@ -0,0 +1,75 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+function Solution(){
+  this._stack = [];
+  this._queue = [];
+
+
+  this.pushCharacter = function(ch) {
+      this._stack[this._stack.length] = ch;
+  }
+
+  this.enqueueCharacter = function(ch) {
+      this._queue.push(ch);
+  }
+
+  this.popCharacter = function() {
+      return this._stack.pop();
+  }
+
+  this.dequeueCharacter = function() {
+      if (typeof this._queue[0] === 'undefined' || this._queue[0] === null) return;
+      var ch = this._queue[0];
+      this._queue = this._queue.slice(1);
+      return ch;
+  }
+}
+
+function main(){
+    // read the string s
+    var s=readLine();
+    var len=s.length;
+    // create the Solution class object p
+    var obj=new Solution();
+    //push/enqueue all the characters of string s to stack
+    for(var i=0;i<len;i++){
+        obj.pushCharacter(s.charAt(i));
+        obj.enqueueCharacter(s.charAt(i));
+    }
+
+    var isPalindrome=true;
+    /*
+    pop the top character from stack
+    dequeue the first character from queue
+    compare both the characters*/
+
+    for(var i=0;i<len/2;i++){
+        if(obj.popCharacter()!=obj.dequeueCharacter()){
+            isPalindrome=false;
+          	break;
+        }
+    }
+    //finally print whether string s is palindrome or not
+
+    if(isPalindrome)
+        console.log("The word, "+s+", is a palindrome.");
+    else
+        console.log("The word, "+s+", is not a palindrome.");
+}
